ca927630578ac39eee038954087a4acbc3c8abc4
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-26 17:29:41 +0900

    テスト実装

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
+  preset: 'jest-puppeteer',
   moduleFileExtensions: ['js', 'ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
-  testMatch: ['<rootDir>/test/**/*.+(ts|js)']
+  testMatch: ['<rootDir>/test/**/*.+(ts|js)'],
 };
--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,5 +1,9 @@
 describe('Sample test', () => {
-  it('calc', () => {
-    expect(1 + 2).toBe(3);
+  beforeAll(async () => {
+    await page.goto('https://google.com');
+  });
+
+  it('should be titled "Google"', async () => {
+    await expect(page.title()).resolves.toMatch('Google');
   });
 });
