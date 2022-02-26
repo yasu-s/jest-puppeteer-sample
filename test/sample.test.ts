be08da54c85d10cad2812caa0790003e0c44f3e9
Yasuyuki Saito <ya_saito@uluru.jp>
2022-02-26 17:55:52 +0900

    typeケース実装

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,10 +1,17 @@
 describe('Sample test', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     await page.goto('http://localhost:8000');
   });
 
-  it('should be titled "test"', async () => {
+  it('title = test', async () => {
     const title = await page.title();
     expect(title).toBe('test');
+  });
+
+  it('type test', async () => {
+    const inputText = 'hoge';
+    await page.type('#txt', inputText);
+    const actual = await page.$eval('input[id="txt"]', (el) => (el as HTMLInputElement).value);
+    expect(actual).toBe(inputText);
   });
 });
